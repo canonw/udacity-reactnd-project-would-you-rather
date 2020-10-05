e53a1ea1cfc11f31f79fe7de514aac32be11487c
Ken Wong <canonw@users.noreply.github.com>
2020-10-05 09:14:15 +0000

    Add login page

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,34 @@
 import React, { Component, Fragment } from "react";
-import PropTypes from "prop-types";
 import { BrowserRouter as Router } from "react-router-dom";
 import { connect } from "react-redux";
 import { handleInitialData } from "../actions/shared";
 import LoadingBar from "react-redux-loading-bar";
 
+import Login from "./Login";
+
 class App extends Component {
   componentDidMount() {
-    this.props.dispatch(handleInitialData());
+    const { dispatch } = this.props;
+    dispatch(handleInitialData());
   }
 
   render() {
+    const { isAuthenticated } = this.props;
+
     return (
       <Router>
         <Fragment>
           <LoadingBar />
-          <div>App</div>
+          {isAuthenticated ? <div>App</div> : <Login />}
         </Fragment>
       </Router>
     );
   }
 }
 
-App.propTypes = {
-  isLoggedIn: PropTypes.bool.isRequired,
-};
-
 function mapStateToProps({ authedUser }) {
   return {
-    isLoggedIn: authedUser === null,
+    isAuthenticated: authedUser !== null && authedUser !== "",
   };
 }
 
