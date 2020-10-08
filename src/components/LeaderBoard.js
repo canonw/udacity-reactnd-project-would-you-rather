d2ab0c5558e9fe235fa58d523b8b018944592db4
Ken Wong <canonw@users.noreply.github.com>
2020-10-08 20:32:04 +0000

    Implement leader board ui

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,26 +1,65 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Grid, Image, Label, Header, Menu } from "semantic-ui-react";
 
 const LeaderBoard = (props) => {
   const { users } = props;
 
-  const score = Object.keys(users)
+  const scoreboard = Object.keys(users)
     .map((uid) => {
       const user = users[uid];
-      const answeredCount = user ? Object.keys(user.answers).length : 0;
-      const createdCount = user ? Object.keys(user.questions).length : 0;
+      const questionAnswered = user ? Object.keys(user.answers).length : 0;
+      const questionCreated = user ? Object.keys(user.questions).length : 0;
       return {
         id: uid,
-        name: user ? user.name : "",
-        answeredCount: answeredCount,
-        createdCount: createdCount,
-        totalCount: answeredCount + createdCount,
+        userName: user ? user.name : "",
+        avatarURL: user ? user.avatarURL : "",
+        questionAnswered: questionAnswered,
+        questionCreated: questionCreated,
+        totalScore: questionAnswered + questionCreated,
       };
     })
-    .sort((a, b) => b.totalCount - a.totalCount);
+    .sort((a, b) => b.totalScore - a.totalScore);
 
-  console.log(score);
-  return <p>Hello LeaderBoard</p>;
+  return (
+    <div className="ui two column centered grid">
+      <div className="column">
+        <Grid celled>
+          {scoreboard.map((score, index) => (
+            <Grid.Row key={score.id}>
+              <Grid.Column width={8}>
+                <Header as="h2">
+                  <Image src={score.avatarURL} /> {score.userName}
+                </Header>
+                <Menu compact>
+                  <Menu.Item as="a">
+                    Question Answered
+                    <Label floating>{score.questionAnswered}</Label>
+                  </Menu.Item>
+                  <Menu.Item as="a">
+                    Created
+                    <Label floating>{score.questionAnswered}</Label>
+                  </Menu.Item>
+                </Menu>
+              </Grid.Column>
+              <Grid.Column width={4}>
+                <Menu vertical>
+                  <Menu.Item as="a">
+                    Rank
+                    <Label color="teal">{index + 1}</Label>
+                  </Menu.Item>
+                  <Menu.Item as="a" position="right">
+                    Total Score
+                    <Label>{score.totalScore}</Label>
+                  </Menu.Item>
+                </Menu>
+              </Grid.Column>
+            </Grid.Row>
+          ))}
+        </Grid>
+      </div>
+    </div>
+  );
 };
 
 function mapStateToProps({ users }) {
